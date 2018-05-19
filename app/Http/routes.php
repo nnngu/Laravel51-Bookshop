27e6df6d2cbe7a8a52ab0cb0d530f556e978f128
nnngu <654015592@qq.com>
2018-05-19 22:28:28 +0800

    send SMS use CCPRestSDK   容联云通讯

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -253,6 +253,7 @@ Route::get('/login', 'View\MemberController@toLogin');
 Route::get('/register', 'View\MemberController@toRegister');
 
 Route::any('service/validate_code/create', 'Service\ValidateController@create');
+Route::any('service/validate_phone/send', 'Service\ValidateController@sendSMS');
 
 
 
